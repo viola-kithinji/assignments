7eb0df677876146592e7a59c0697b3f7d5c2d49b
violakithinji <kithinjiviola@gmail.com>
2021-10-14 20:51:53 +0300

    python assignment 1_1.py

---
+++ assignment 1_1.py
@@ -0,0 +1,7 @@
+def main():
+    print("hello world!")
+    print("hello again")
+    print('yay!printing.')
+    print("i'd much rather you 'not'.")
+    print('i "said" do not touch this.')
+main()
